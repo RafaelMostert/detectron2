86a8de34c541d41bcabd10492badaa57cb797be7
Rafael Mostert Tritanium <mostert@strw.leidenuniv.nl>
2020-06-25 14:44:38 +0200

    changed filenames

--- precomputed_datasetsize_experiments.sh
+++ precomputed_datasetsize_experiments.sh
@@ -5,9 +5,9 @@ G4=13
 G5=14
 G6=15
 
-CUDA_VISIBLE_DEVICES=$G1 python train_lofar.py configs/lofar_detection/datasetsizes_v17_10.yaml &
-CUDA_VISIBLE_DEVICES=$G2 python train_lofar.py configs/lofar_detection/datasetsizes_v17_100.yaml &
-CUDA_VISIBLE_DEVICES=$G3 python train_lofar.py configs/lofar_detection/datasetsizes_v17_1000.yaml &
-CUDA_VISIBLE_DEVICES=$G4 python train_lofar.py configs/lofar_detection/datasetsizes_v17_2000.yaml &
-CUDA_VISIBLE_DEVICES=$G5 python train_lofar.py configs/lofar_detection/datasetsizes_v17_3000.yaml &
-CUDA_VISIBLE_DEVICES=$G6 python train_lofar.py configs/lofar_detection/datasetsizes_v17_all.yaml &
+CUDA_VISIBLE_DEVICES=$G1 python train_lofar.py configs/lofar_detection/precomputed_datasetsizes_v18_10.yaml &
+CUDA_VISIBLE_DEVICES=$G2 python train_lofar.py configs/lofar_detection/precomputed_datasetsizes_v18_100.yaml &
+CUDA_VISIBLE_DEVICES=$G3 python train_lofar.py configs/lofar_detection/precomputed_datasetsizes_v18_1000.yaml &
+CUDA_VISIBLE_DEVICES=$G4 python train_lofar.py configs/lofar_detection/precomputed_datasetsizes_v18_2000.yaml &
+CUDA_VISIBLE_DEVICES=$G5 python train_lofar.py configs/lofar_detection/precomputed_datasetsizes_v18_3000.yaml &
+CUDA_VISIBLE_DEVICES=$G6 python train_lofar.py configs/lofar_detection/precomputed_datasetsizes_v18_all.yaml &
