7aa7a02eb08d64e265243099ea078be316509c61
Rafael Mostert Tritanium <mostert@strw.leidenuniv.nl>
2020-09-05 00:29:39 +0200

    add a number of config files

--- iteration_experiments_no_rotation.sh
+++ iteration_experiments_no_rotation.sh
@@ -1,10 +1,10 @@
-G1=13
-G2=14
-G3=15
+G1=12
+G2=13
+G3=14
 
-CUDA_VISIBLE_DEVICES=$G1 python train_lofar.py \
-    configs/lofar_detection/iterations_v10_100kconstantLR.yaml > v10.txt &
 CUDA_VISIBLE_DEVICES=$G2 python train_lofar.py \
     configs/lofar_detection/iterations_v12_100kstepLR.yaml > v12.txt &
 CUDA_VISIBLE_DEVICES=$G3 python train_lofar.py \
-    configs/lofar_detection/iterations_v14_100kcosineLR.yaml v13.txt &
+    configs/lofar_detection/iterations_v14_100kcosineLR.yaml > v14.txt &
+#CUDA_VISIBLE_DEVICES=$G1 python train_lofar.py \
+#    configs/lofar_detection/iterations_v10_100kconstantLR.yaml > v10.txt &
